ddc077267b7e3c65ba7fc23bb8c31fbb40629b67
Mecha Labs <91070944+Mecha-Labs@users.noreply.github.com>
2021-10-02 07:09:58 +0700

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,7 +6,7 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned && !m.isGroup) {
     if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
     if (!m.text) return
-    let res = await fetch('https://api.simsimi.net/v2/?text=${encodeURIComponent (m.text)}&lc=id&cf=true')
+    let res = await fetch('https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=id&cf=true')
     if (!res.ok) return m.reply(eror)
     let json = await res.json()
     await m.reply(res.data.messages[response])
